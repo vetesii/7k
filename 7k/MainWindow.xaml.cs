db5b1e4ff8f2d0e5d69d638fb8e9908d4c0f6c2e
vetesii <vetesi.istvan@gmail.com>
2014-10-17 14:53:44 +0200

    Copy sukram gui
    From:
    http://www.codeproject.com/Articles/23265/WPF-Diagram-Designer-Part

--- 7k/MainWindow.xaml.cs
+++ 7k/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using _7k.Model.ContextElement.Task;
 using _7k.Model.ContextElement.Task.InnerDotNet;
+using _7k.UserInterface;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -29,6 +30,47 @@ namespace _7k
             InitializeComponent();
 
             AbstractTask at = new IdnRemoveEmptyParagraphBeforeAndAfterStyles();
+        }
+
+        private void OnClickNew(object sender, RoutedEventArgs args)
+        {
+            MyDesignerCanvas.Children.Clear();
+        }
+
+        private void OnClickRotateLeft(object sender, RoutedEventArgs args)
+        {
+            Rotate(-90);
+        }
+
+        private void OnClickRotateRight(object sender, RoutedEventArgs args)
+        {
+            Rotate(90);
+        }
+
+        // sort of a hack, only values of 90 (right) or -90 (left) make sense
+        // for demo purposes only
+        private void Rotate(double angle)
+        {
+            foreach (DesignerItem item in MyDesignerCanvas.SelectedItems)
+            {
+                FrameworkElement element = item.Content as FrameworkElement;
+                if (element != null)
+                {
+                    RotateTransform rotateTransform = element.LayoutTransform as RotateTransform;
+                    if (rotateTransform == null)
+                    {
+                        rotateTransform = new RotateTransform();
+                        element.LayoutTransform = rotateTransform;
+                    }
+
+                    rotateTransform.Angle = (rotateTransform.Angle + angle) % 360;
+                    Canvas.SetLeft(item, Canvas.GetLeft(item) - (item.Height - item.Width) / 2);
+                    Canvas.SetTop(item, Canvas.GetTop(item) - (item.Width - item.Height) / 2);
+                    double width = item.Width;
+                    item.Width = item.Height;
+                    item.Height = width;
+                }
+            }
         }
 
         private void onDragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
---
+++ 7k/UserInterface/Adorners/ResizeChrome.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace _7k.UserInterface
+{
+    public class ResizeChrome : Control
+    {
+        static ResizeChrome()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(ResizeChrome), new FrameworkPropertyMetadata(typeof(ResizeChrome)));
+        }
+    }
+}
